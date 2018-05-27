cb5e7b00bfc566eb72a28443d72239ac54a323fd
James Leong <jyleong@sfu.ca>
2018-05-27 15:07:29 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import 'typeface-roboto'
+import { orange } from '@material-ui/core/colors'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import { render } from 'react-dom'
+import App from './App'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: orange[200], // same as '#FFCC80',
+      main: '#FB8C00', // same as orange[600]
+      dark: '#EF6C00',
+      contrastText: 'rgb(0,0,0)'
+    }
+  }
+})
+
+render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+)
